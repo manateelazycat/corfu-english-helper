515d76f1a1daaf3f4445c8052fb1ad1df8c14da2
Andy Stewart <lazycat.manatee@gmail.com>
2022-05-13 20:41:36 +0800

    Alignment translation content.

--- corfu-english-helper.el
+++ corfu-english-helper.el
@@ -16,9 +16,34 @@
   "The status of corfu-english-helper plugins.
 Default is disable.")
 
-(defun corfu-english-helper-annotation (s)
-  (let* ((translation (get-text-property 0 :initials s)))
-    (format "  %s" translation)))
+(defvar corfu-english-helper-last-showing-candidate-max-index nil)
+(defvar corfu-english-helper-current-input nil)
+(defvar corfu-english-helper-translation-max-width nil)
+
+(defun corfu-english-helper-translation-max-width ()
+  (let ((showing-candidate-max-index (min (+ corfu-count corfu--scroll) corfu--total))
+        (showing-candidate-min-index corfu--scroll))
+    (if (and (eq corfu-english-helper-current-input corfu--input)
+             (eql showing-candidate-max-index corfu-english-helper-last-showing-candidate-max-index))
+        corfu-english-helper-translation-max-width
+      (progn
+        (setq corfu-english-helper-last-showing-candidate-max-index showing-candidate-max-index)
+        (setq corfu-english-helper-current-input corfu--input)
+        (setq showing-candidates (butlast (nthcdr showing-candidate-min-index corfu--candidates) (- corfu--total showing-candidate-max-index)))
+        (setq corfu-english-helper-translation-max-width
+              (apply 'max (mapcar (lambda (w)
+                                    (string-width (get-text-property
+                                                   0
+                                                   :initials (assoc-string (downcase w) corfu-english-helper-completions))))
+                                  showing-candidates)))))))
+
+(defun corfu-english-helper-annotation (candidate)
+  (let* ((translation (get-text-property 0 :initials candidate))
+         (translation-width (string-width translation))
+         (corfu--candidates-length (length corfu--candidates))
+         (max-translation-width (corfu-english-helper-translation-max-width))
+         (blank-width (- max-translation-width translation-width)))
+    (format "    %s" (concat translation (make-string (max 0  (- max-translation-width translation-width)) ?\s)))))
 
 (defun corfu-english-helper-search (&optional interactive)
   (interactive (list t))
@@ -65,22 +90,22 @@ Default is disable.")
         ;; Restore options.
         (setq-local corfu-auto corfu-english-helper--corfu-auto)
         (setq-local corfu-auto-prefix corfu-english-helper--corfu-auto-prefix)
-        
+
         (setq completion-at-point-functions (remove 'corfu-english-helper-search completion-at-point-functions))
         (setq corfu-english-helper-active-p nil)
         (message "Corfu english helper has disable."))
-    
+
     ;; Save options.
     (setq-local corfu-english-helper--corfu-auto corfu-auto)
     (setq-local corfu-english-helper--corfu-auto-prefix corfu-auto-prefix)
-    
+
     ;; Turn on `corfu-auto' and adjust `corfu-auto-prefix' to 0.
     (setq-local corfu-auto t)
     (setq-local corfu-auto-prefix 0)
-    
+
     ;; We need call `(setq-local corfu-auto t)' before corfu-mode turn on.
     (corfu-mode 1)
-    
+
     (add-to-list 'completion-at-point-functions #'corfu-english-helper-search)
     (setq corfu-english-helper-active-p t)
     (message "Corfu english helper has enable.")))
